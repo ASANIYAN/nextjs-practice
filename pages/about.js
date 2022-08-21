eaa511e64af8181bd8a44d59e753fd70c8192820
Ayomide <heritageayomide2017@gmail.com>
2022-08-21 20:24:06 +0100

    finished practice project

---
+++ pages/about.js
@@ -0,0 +1,23 @@
+import Head from "next/head";
+
+ const About = () => {
+    return (
+        <>
+            <Head>
+            <title>Ninja List | About </title>
+            <meta name="keywords" content="ninjas" />
+            </Head>
+            <div>
+                <h1>
+                    About
+                </h1>
+                <p>Lorem, ipsummmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmm</p>
+                <p>lorem ipsummmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmm</p>
+                <p>lorem ipsummmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmm</p>
+                <p>lorem ipsummmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmm</p>
+            </div>
+        </>
+    );
+}
+ 
+export default About;
---
+++ pages/ninjas/index.js
@@ -0,0 +1,35 @@
+import Link from 'next/link';
+import styles from '../../styles/Ninjas.module.css';
+
+//never runs in the browser, only at build time
+export const getStaticProps = async() => {
+
+    const res = await fetch('https://jsonplaceholder.typicode.com/users');
+    const data = await res.json();
+
+    return {
+        props: { ninjas: data }
+    }
+
+}
+
+const Ninjas = ({ ninjas}) => {
+    return (
+        <div>
+            <h1>All Ninjas</h1>
+            {
+                ninjas.map(ninja => (
+                    <Link href={"/ninjas/" + ninja.id} key={ninja.id}>
+                        <a className={styles.single}>
+                            <h3>
+                                { ninja.name }
+                            </h3>
+                        </a>
+                    </Link>
+                ))
+            }
+        </div>
+    );
+}
+ 
+export default Ninjas;
